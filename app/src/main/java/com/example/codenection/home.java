7143c6e28fa554acc76d5d81bf01feb566288422
miracle1116 <yiquan1116@gmail.com>
2022-11-14 00:14:50 +0800

    hellohello

--- app/src/main/java/com/example/codenection/home.java
+++ app/src/main/java/com/example/codenection/home.java
@@ -3,8 +3,10 @@ package com.example.codenection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class home extends AppCompatActivity {
+    private TextView hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
